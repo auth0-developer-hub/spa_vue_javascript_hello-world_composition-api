3044b5ebe44916540dfaad9702094f21200aa5d9
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2023-07-24 23:40:05 -0500

    implement authentication with external api integration

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ app
       domain: import.meta.env.VITE_AUTH0_DOMAIN,
       clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
       authorizationParams: {
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
         redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
       },
     })
